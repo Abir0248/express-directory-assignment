822154fe27682540441efdf85f0d36e185df753a
Abir0248 <abir15-6789@diu.edu.bd>
2023-09-12 01:25:47 +0600

    up

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-// App.js
 const express = require("express");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const hpp = require("hpp");
 
-// Loading environment variables
 dotenv.config({ path: "./config.env" });
 
 const app = express();
 
-// Middlewares for security and data processing
 app.use(helmet());
 app.use(hpp());
-// Add other security middlewares as required
 
-// Routes
 app.use("/api", require("./src/Routes/api"));
 
-// Undefined routes
 app.all("*", (req, res) => {
   res.status(404).json({
     status: "fail",
